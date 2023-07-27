e63088fa33e101a542e1446a0ffa4e09747edc44
erickNjuki <njukierick@gmail.com>
2023-07-27 21:59:15 +0300

    startup files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Product from "./components/Product";
+import products from "./data/products.json";
+
+function App() {
+  return (
+    <div>
+      <div>
+        {products.map((product) => (
+          <Product product={product} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App
