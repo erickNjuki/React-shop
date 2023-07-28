caaf9500e699cf0bcbd653a820ecd81b03464452
erickNjuki <njukierick@gmail.com>
2023-07-28 09:39:14 +0300

    Added Components Typography

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,9 +7,9 @@ function Product({product: {title, price, description, category, image}}) {
         <div style={{backgroundImage: `url(${image})`}}className="w-5/12"></div>
       <div className="w-7/12 p-5" >
         <h1>{title.substr(0, 50)}</h1>
-        <h3>{category}</h3>
-        <p>{price}$</p>
-        <div>{description.substr(0, 100)}</div>
+        <h3 className="text-gray-400">{category}</h3>
+        <p className="text-red-400 mt-4">{price}$</p>
+        <div className="mt-4">{description.substr(0, 100)}</div>
       </div>
     </div>
   )
